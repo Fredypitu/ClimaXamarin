5d3a04004ae385883bc23c0b4f7b549c3f5cf69a
Alfredo <alfredofedericod@gamil.com>
2020-11-14 00:35:40 -0300

    Carpetas creadas: Clases, Paginas y Servicios

---
+++ ClimaXamarin/ClimaXamarin/Clases/Clima.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClimaXamarin.Clases
+{
+    class Clima
+    {
+        public int MyProperty { get; set; }
+    }
+}
