b82dbb43c6fc3a40cf41a88e1c8e10f59b9f24cd
Alfredo <alfredofedericod@gamil.com>
2020-11-14 02:37:33 -0300

    Dos Clases nuevas: Clima y ServicioClima

--- ClimaXamarin/ClimaXamarin/Clases/Clima.cs
+++ ClimaXamarin/ClimaXamarin/Clases/Clima.cs
@@ -4,8 +4,20 @@ using System.Text;
 
 namespace ClimaXamarin.Clases
 {
-    class Clima
+    public class Clima
     {
-        public int MyProperty { get; set; }
+        public Clima()
+        {
+            this.Titulo = string.Empty;
+            this.Temperatura = string.Empty;
+            this.Viento = string.Empty;
+            this.Humedad = string.Empty;
+            this.Visibilidad = string.Empty;
+        }
+        public string Titulo { get; set; }
+        public string Temperatura { get; set; }
+        public string Viento { get; set; }
+        public string Humedad { get; set; }
+        public string Visibilidad { get; set; }
     }
 }
---
+++ ClimaXamarin/ClimaXamarin/Servicios/ServicioClima.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ClimaXamarin.Clases;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ClimaXamarin.Servicios
+{
+    public static class ServicioClima
+    {
+        static readonly string key = "1be67513305e2ea435c7c0fbdfb244a4";
+        public static async Task<Clima> ConsultarClima(string pCiudad)
+        {
+            var conexion = $"http://api.openweathermap.org/data/2.5/weather?q={pCiudad}&appid={key}";
+
+            using (var cliente = new HttpClient())
+            {
+                var peticion = await cliente.GetAsync(conexion);
+                if (peticion != null)
+                {
+                    var json = await peticion.Content.ReadAsStringAsync();
+                    var datos = (JContainer)JsonConvert.DeserializeObject(json);
+                    if (datos["weather"]!=null)
+                    {
+                        var clima = new Clima();
+                        clima.Titulo = (string)datos["name"];
+                        clima.Temperatura = ((float)datos["main"]["temp"] - 273.15).ToString("N2") + " °C";
+                        clima.Viento = (string)datos["wind"]["speed"] + " mph";
+                        clima.Humedad = (string)datos["main"]["humidity"] + " %";
+                        clima.Visibilidad = (string)datos["weather"][0]["main"];
+
+                        return clima;
+                    }
+                }
+            }
+            return default;
+        }
+    }
+}
